c4f23c7b9caa9632a98d3254eaaab3d0b33c7a6a
Freedy <21914103@zju.edu.cn>
2020-08-23 15:16:45 +0800

    Home Page 样式完成

--- src/view/Money.tsx
+++ src/view/Money.tsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import styled from 'styled-components'
+require('icon/Money.svg')
 
 const Wrapper = styled.section`
     background:rgb(40,44,52);
@@ -9,6 +10,9 @@ const Wrapper = styled.section`
         display:flex;
         justify-content:space-between;
         padding:10px 10px;
+        > .in{
+            color:rgb(40,44,52);
+        }
     }
     > main{
         display:flex;
@@ -23,26 +27,57 @@ const Wrapper = styled.section`
     }
 `
 const IconWrapper = styled.div`
-    
+    position:relative;
+    > div{
+        background:white;
+        left:50%;
+        top:-50px;
+        transform:translate(-50%);
+        position:absolute;
+        border-radius:60px;
+        > div{
+            color:rgb(97,218,251);
+            display:flex;
+            justify-content;center;
+            align-item:center;
+            flex-direction:column;
+            border:2px solid rgb(97,218,251);
+            margin:10px;
+            border-radius:60px;
+            padding:10px 24px;
+            > span{
+                margin:5px 0;
+            }
+        }
+    }
 `
 
 const Money = () => {
     return (
-        <Wrapper>
-            <header>
-                <span>2020-8</span>
-                <span>本月结余</span>
-                <span>2020-8</span>
-            </header>
-            <main>Money</main>
-            <footer>
-                <span>收入:xxx</span>
-                <span>支出:xxx</span>
-            </footer>
+        <>
+            <Wrapper>
+                <header>
+                    <span>2020-8</span>
+                    <span>本月结余</span>
+                    <span className="in">2020-8</span>
+                </header>
+                <main>Money</main>
+                <footer>
+                    <span>收入:xxx</span>
+                    <span>支出:xxx</span>
+                </footer>
+            </Wrapper >
             <IconWrapper>
-                <div></div>
+                <div>
+                    <div>
+                        <svg className="icon">
+                            <use xlinkHref="#Money" />
+                        </svg>
+                        <span>记一笔</span>
+                    </div>
+                </div>
             </IconWrapper>
-        </Wrapper>
+        </>
     )
 }
 
